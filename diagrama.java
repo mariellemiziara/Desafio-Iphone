56a05d3868b64bbeb201c9a0668057be753de2eb
mariellemiziara <63522352+mariellemiziara@users.noreply.github.com>
2024-05-20 11:52:25 -0300

    Update diagrama.java

---
+++ diagrama.java
@@ -0,0 +1,22 @@
+public class diagrama {
+    class ReprodutorMusical {
+        +tocar()
+        +pausar()
+        +selecionarMusica(musica: String)
+    }
+
+    class AparelhoTelefonico {
+        +ligar(numero: String)
+        +atender()
+        +iniciarCorreioVoz()
+    }
+
+    class NavegadorInternet {
+        +exibirPagina(url: String)
+        +adicionarNovaAba()
+        +atualizarPagina()
+    }
+
+    ReprodutorMusical --|> AparelhoTelefonico
+    ReprodutorMusical --|> NavegadorInternet
+}
